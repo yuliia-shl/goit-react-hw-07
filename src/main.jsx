fc014a445cfc1848aa2876f76db27ff63d9d7bf0
Yuli <yu.expert@gmail.com>
2024-12-15 14:46:08 +0200

    Delete persistor

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,12 @@ import 'modern-normalize';
 import './index.css';
 import App from './components/App.jsx';
 import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store.js';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate loading={<p>Loading...</p>} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </StrictMode>
 );
